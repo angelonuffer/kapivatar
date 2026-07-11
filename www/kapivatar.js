29c4862f407fb6f25b7066019e4c9c25a41c96cd
Ângelo Otávio Nuffer Nunes <angelonuffer@gmail.com>
2026-07-11 22:03:56 +0000

    Altera algoritmo de geração de chaves para usar RSA-OAEP em vez de RSASSA-PKCS1-v1_5

--- www/kapivatar.js
+++ www/kapivatar.js
@@ -20,13 +20,13 @@ const gerar_hash = async (conteudo) => {
 const gerar_chaves = async () => {
   return await crypto.subtle.generateKey(
     {
-      name: "RSASSA-PKCS1-v1_5",
+      name: "RSA-OAEP",
       modulusLength: 2048,
       publicExponent: new Uint8Array([1, 0, 1]),
       hash: "SHA-256",
     },
     true,
-    ["sign", "verify"]
+    ["encrypt", "decrypt"]
   )
 }
 
